70f7ebdf771cf6035bd0e0feb3cc4fe0b4be2169
Gleb Lebedev <gleb@gleblebedev.com>
2020-04-12 00:43:37 +0100

    Avalonia UI editor prototype

---
+++ src/Toe.SPIRV.CrossPlatformNodeEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Toe.Scripting.Avalonia.ViewModels;
+
+namespace Toe.SPIRV.CrossPlatformNodeEditor.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            Script = new ScriptViewModel();
+        }
+        public ScriptViewModel Script { get; set; }
+    }
+}
